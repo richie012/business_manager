bf77e895de04668b5104005ca8a3ee6d78532347
richie <82734398+richie012@users.noreply.github.com>
2022-04-04 20:57:36 +0500

    Add files via upload

---
+++ business_manager/src/test/java/com/example/business_manager/BusinessManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.business_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
