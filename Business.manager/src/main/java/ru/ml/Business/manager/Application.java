56aa32801791d48da0ac721d9f103bfa00bb60aa
Алеkсий <my.beck.for.work@mail.ru>
2022-04-22 10:16:38 +0500

    Удаление лишнего, для большой переделки

--- Business.manager/src/main/java/ru/ml/Business/manager/Application.java
+++ Business.manager/src/main/java/ru/ml/Business/manager/Application.java
@@ -2,17 +2,11 @@ package ru.ml.Business.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@EnableJpaRepositories("ru.ml.Business.manager.repository")
-@EntityScan("ru/ml/Business.manager/entity")
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
--- Business.manager/src/main/java/ru/ml/Business/manager/entity/Businessmen.java
+++
@@ -1,6 +0,0 @@
-package ru.ml.Business.manager.entity;
-
-public interface Businessmen {
-    String getName();
-    Long getId();
-}
