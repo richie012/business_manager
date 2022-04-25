723600b554e0899134e4072b3b5fad1927bad5c4
richie <82734398+richie012@users.noreply.github.com>
2022-04-25 15:35:32 +0500

    Delete Business.manager directory

--- Business.manager/src/main/java/ru/ml/Business/manager/repository/BusinessmenRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.ml.Business.manager.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.ml.Business.manager.entity.UserBusinessmen;
-public interface BusinessmenRepository extends CrudRepository<UserBusinessmen, Long>
-{
-}
--- Business.manager/src/main/java/ru/ml/Business/manager/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ml.Business.manager.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.ml.Business.manager.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-}
-
